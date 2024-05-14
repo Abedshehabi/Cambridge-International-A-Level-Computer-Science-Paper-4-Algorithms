9b0c49dc7ef1107fbd42d7aeff32107e36dbd36f
Abedshehabi <53215429+Abedshehabi@users.noreply.github.com>
2024-05-14 23:16:09 +0400

    Add files via upload

---
+++ Paper 4.py
@@ -0,0 +1,18 @@
+import random
+
+numbers = [44, 10, 35, 2, 38, 95, 43, 52, 30, 73]
+file = 
+
+#Linear Search
+#Binary Search
+
+
+Binary search only works if the list is sorted.
+Worst case time complexity of binary search is O(log N) and worst case space complexity is O(1)
+
+#Bubble Sort
+#Insertion sort
+#Reading from a file
+#Writing to a file
+
+
